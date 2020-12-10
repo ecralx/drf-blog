3f8e0cc516e7fde8422b2178dca219b62258cfdc
Achraf Gharbi <achraf.ghb@gmail.com>
2020-12-10 15:19:46 +0100

    Add django admin

--- blog/authentication/admin.py
+++ blog/authentication/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from blog.authentication.models import CustomUser
+
 
 # Register your models here.
+@admin.register(CustomUser)
+class CustomUserAdmin(admin.ModelAdmin):
+    pass
--- blog/settings.py
+++ blog/settings.py
@@ -125,7 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = "/static/"
-
+STATIC_ROOT = BASE_DIR / "static"
 
 # Custom setup
 # Verify that rest_framework is in installed apps
